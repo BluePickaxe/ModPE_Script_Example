39467e6575a394f70a4e8ab312e7b9eb266584cc
푸른곡괭이 <sungwanim@gmail.com>
2016-10-08 18:25:18 +0900

    Update WorldEdit.js

--- WorldEdit/WorldEdit.js
+++ WorldEdit/WorldEdit.js
@@ -8,7 +8,7 @@ if(i==271)
 pos1[0]=x;
 pos1[1]=y;
 pos1[2]=z;
-clientMessage("pos1이 설정되었습니다. X: "+x+" Y: "+y+" Z: "+z);
+clientMessage("pos1 selected. X: "+x+" Y: "+y+" Z: "+z);
 }
 }
 
@@ -19,7 +19,7 @@ if(getCarriedItem() == 271)
 pos2[0]=x;
 pos2[1]=y;
 pos2[2]=z;
-clientMessage("pos2가 설정되었습니다. X: "+x+" Y: "+y+" Z: "+z);
+clientMessage("pos2 selected. X: "+x+" Y: "+y+" Z: "+z);
 preventDefault();
 }
 }
@@ -34,9 +34,7 @@ var startZ=Math.min(pos1[2], pos2[2]);
 var endZ = Math.max(pos1[2], pos2[2]);
 
 var Data = cmd.split(" ");
-if(Data[0]=="채우기")
-{
-if(typeof(Data[1]) == "number"||typeof(Data[2]) == "number")
+if(Data[0]=="fill")
 {
 for(var x=startX;x <= endX;x++)
 {
@@ -49,7 +47,5 @@ setTile(x, y, z, Data[1], Data[2]);
 }
 }
 clientMessage("블럭이 변경되었습니다. "+Data[1]+": "+Data[2]);
-} else {
-clientMessage("아이템코드에는 숫자만 입력할 수 있습니다.");
 }
 }
